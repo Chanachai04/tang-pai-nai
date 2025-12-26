52e1750a0b4ea0a2b245581b432f8582e18d600f
Chanachai04 <chn.benmat@gmail.com>
2025-12-26 20:56:34 +0700

    fix:docker have hot reload

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-center text-2xl">Tang Pai Nais</h1>
+      <h1 className="text-center text-2xl">Tang Pai Naiss</h1>
     </div>
   );
 }
